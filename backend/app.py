918742f3e0f7bdbd6ceeb2ad9d7102d851c46cbb
SinvalFilho <sinvalleitefilho@gmail.com>
2024-09-25 03:04:14 -0300

    1.1

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route('/')
 def home():
-    return jsonify({'message': 'O backend está funcionando !!!'})
+    return jsonify({'message': 'O backend está funcionando !'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
